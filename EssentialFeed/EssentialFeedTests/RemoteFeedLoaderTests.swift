1f521599dbbb17e49abf8183a21fae98b9ff45b2
Areej Sadaqa <areejsadaqa66@gmail.com>
2026-04-20 22:19:45 +0300

    simplify our Spy

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -14,7 +14,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestsDataFromURL() {
         let (_, client) = makeSUT()
         
-        XCTAssertNil(client.requestedURL)
+        XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
     func test_load_requestsDataFromURL() {
@@ -26,7 +26,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         sut.load()
         
         //Assert
-        XCTAssertEqual(client.requestedURL, url)
+        XCTAssertEqual(client.requestedURLs, [url])
     }
     
     func test_loadTwice_requestsDataFromURLTwice() {
@@ -52,11 +52,9 @@ final class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private class HTTPClientSpy: HTTPClient {
-        var requestedURL: URL?
         var requestedURLs = [URL]()
         
         func get(from url: URL) {
-            requestedURL = url
             requestedURLs.append(url)
         }
     }
