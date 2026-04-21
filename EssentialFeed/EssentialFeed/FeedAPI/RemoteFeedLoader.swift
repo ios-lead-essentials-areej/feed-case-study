6d93a7f43703dcc538ca5bd9b2c302e467f098b0
Areej Sadaqa <areej.sadaqa@thechefz.co>
2026-04-21 21:24:17 +0300

    Reapply "Add RemoteFeedLoader Result type with success/failure cases"
    This reverts commit 6539efb43924e6f6d927548bbc6f6056266441a2.

--- EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -27,20 +27,25 @@ public final class RemoteFeedLoader {
         case invalidData
     }
     
+    public enum Result: Equatable {
+        case success([FeedItem])
+        case failure(Error)
+    }
+    
     public init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
     }
     
-    public func load(completion: @escaping (Error) -> Void) {
+    public func load(completion: @escaping (Result) -> Void) {
         ///mapping the domain error with our enum type error
         client.get(from: url) { result in
             
             switch result {
             case .success:
-                completion(.invalidData)
+                completion(.failure(.invalidData))
             case .failure:
-                completion(.connectivity)
+                completion(.failure(.connectivity))
             }
         }
     }
