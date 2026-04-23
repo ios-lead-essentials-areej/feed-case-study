8d1d4aefd6a051776c7eb7e215bf039cb4c0af71
Areej Sadaqa <areej.sadaqa@thechefz.co>
2026-04-23 21:10:53 +0300

    replace RemoteFeedLoader Result enum with the predefined LoadFeedResult enum from the feed module and this is a preparation to conform to the FeedLoader Protocol

--- EssentialFeed/EssentialFeed/Feed Feature/FeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedLoader.swift
@@ -4,11 +4,19 @@
 
 import Foundation
 
-enum LoadFeedResult {
+///To solve all issue we can make our Error generic and add associated types
+///but we're only making LoadFeedResult equatable just because of the test, and this is a production code
+///complicates alot our production codes , prod has no req of equatable 
+
+public enum LoadFeedResult<Error: Swift.Error> {
 	case success([FeedItem])
-	case error(Error)
+	case failure(Error)
 }
 
+extension LoadFeedResult: Equatable where Error: Equatable {}
+
 protocol FeedLoader {
-	func load(completion: @escaping (LoadFeedResult) -> Void)
+    associatedtype Error: Swift.Error
+    
+	func load(completion: @escaping (LoadFeedResult<Error>) -> Void)
 }
