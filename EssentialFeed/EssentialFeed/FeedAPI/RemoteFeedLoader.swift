692f73dcc95d16bf50836d6b3cf48e745f75158c
Areej Sadaqa <areej.sadaqa@thechefz.co>
2026-04-23 20:51:36 +0300

    guarantee to not deliver a result (invoke the completion closure) after the RemoteFeedLoader instance has been deallocated

--- EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -28,10 +28,16 @@ public final class RemoteFeedLoader {
         self.client = client
     }
     
+    ///I think the RemoteFeedLoader could get deallocated in some scenarios, for example if, in some UI setup, the user dismisses the current view controller while the loading is still in progress.
     public func load(completion: @escaping (Result) -> Void) {
         ///mapping the domain error with our enum type error
-        client.get(from: url) { result in
-            
+        client.get(from: url) { [weak self] result in
+            ///If you don't reference self inside the closure, it won't be retained. So, in this case, self (RemoteFeedLoader) won't be retained.
+            ///However, the client closure retains the closure you passed to the RemoteFeedLoader - and the client instance can outlive the RemoteFeedLoader instance (even though you set the RemoteFeedLoader to nil, the client may still exist in memory).
+            ///So even though the RemoteFeedLoader was deallocated, the client can still exist and hold a reference to your completion closure and can call it on completion.
+            ///
+            guard self != nil else { return } ///without this we will get memory leak and failed test (test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated)
+
             switch result {
             case let .success(data, response):
                 completion(FeedItemMapper.map(data, from: response)) //self here might return a raiain cycle
