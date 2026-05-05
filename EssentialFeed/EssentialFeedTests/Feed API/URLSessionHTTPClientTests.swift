d7fa980c19782ce72e38c5590fb2d969be45f305
Areej Sadaqa <areej.sadaqa@thechefz.co>
2026-05-06 00:33:54 +0300

    Guarantees correct URL and HTTP method on get requests

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -19,7 +19,6 @@ final class URLSessionHTTPClient {
     func get(from url: URL, completionHandler: @escaping (HTTPClientResult) -> Void ) {
         //the issue of our tests now its sensitive to the correct url passing because we intercept only our url, but to enhance we can intercept all reqests regarding the url
         //also we want our assertion to be more precise about the error when they fail.
-        let url = URL(string: "http://google-url.com")!
         session.dataTask(with: url, completionHandler: { _ ,_, error in
             if let error = error {
                 completionHandler(.failure(error))
@@ -29,6 +28,23 @@ final class URLSessionHTTPClient {
 }
 
 final class URLSessionHTTPClientTests: XCTestCase {
+    func test_getFromURL_performsGetRequestWithURL() {
+        URLProtocolStub.startInterceptingRequests()
+        
+        let url = URL(string: "http://any-url.com")!
+        let exp = expectation(description: "wait for completion")
+        
+        URLProtocolStub.observeRequests { request in
+            XCTAssertEqual(request.url, url)
+            XCTAssertEqual(request.httpMethod, "GET")
+            exp.fulfill()
+        }
+        
+        URLSessionHTTPClient().get(from: url, completionHandler: {_ in })
+        
+        wait(for: [exp], timeout: 1.0)
+        URLProtocolStub.stopInterceptingRequest()
+    }
     
     func test_getFromURL_failsOnRequestError() {
         //we need to register a class
@@ -67,6 +83,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
     private class URLProtocolStub: URLProtocol {
         //we can intercept all reqests regarding the url,  no need for url
         private static var stub: Sub?
+        private static var requestObserver: ((URLRequest) -> Void)?
         
         private struct Sub {
             let data: Data?
@@ -78,6 +95,10 @@ final class URLSessionHTTPClientTests: XCTestCase {
             stub = Sub(data: data, response: response, error: error)
         }
         
+        static func observeRequests(observer: @escaping (URLRequest) -> Void) {
+            requestObserver = observer
+        }
+        
         static func startInterceptingRequests() {
             URLProtocol.registerClass(URLProtocolStub.self)
         }
@@ -85,6 +106,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
         static func stopInterceptingRequest() {
             URLProtocol.unregisterClass(URLProtocolStub.self)
             stub = nil
+            requestObserver = nil
         }
         
         //URLProtocol testing network requested is to use the little-known URL Loading System to intercept and handle requests with URLProtocol stubs.
@@ -92,6 +114,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
         
         //this func 'canInit' we return true if we can intercept the network request
         override class func canInit(with request: URLRequest) -> Bool {
+            requestObserver?(request)
             return true
         }
         
