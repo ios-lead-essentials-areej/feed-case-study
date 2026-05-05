5cc7e5992adf33c4a8b93a1b5e9344400d85e632
Areej Sadaqa <areej.sadaqa@thechefz.co>
2026-05-06 00:39:53 +0300

    move the URLSessionHTTPClient (the system under test, SUT) to a factory method to protect our tests or SUT creation from breaking changes

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -51,7 +51,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         
-        URLSessionHTTPClient().get(from: url, completionHandler: {_ in })
+        makeSUT().get(from: url, completionHandler: {_ in })
         
         wait(for: [exp], timeout: 1.0)
     }
@@ -60,12 +60,10 @@ final class URLSessionHTTPClientTests: XCTestCase {
         let url = URL(string: "http://any-url.com")!
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
-        
-        let sut = URLSessionHTTPClient()
-        
+                
         let exp = expectation(description: "wait for completion")
         
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 ///The error and the receivedError are different NSError instances, which is a new behavior on iOS 14+.
@@ -84,6 +82,10 @@ final class URLSessionHTTPClientTests: XCTestCase {
     }
     
     // MARK: - Helpers
+    
+    private func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
+    }
     
     private class URLProtocolStub: URLProtocol {
         //we can intercept all reqests regarding the url,  no need for url
