282493f722fce09f100a4cf0a1225cd7acd9898c
Areej Sadaqa <areej.sadaqa@thechefz.co>
2026-05-16 14:58:57 +0300

    Move LocalFeedItem to its own file

--- EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -14,24 +14,3 @@ public protocol FeedStore {
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
     func insert(_ items: [LocalFeedItem], timestamp: Date, completion: @escaping insertionCompletion)
 }
-
-// we need to separate and make DTO data transfer representayion object to decouple modules and centrelization
-
-public struct LocalFeedItem: Equatable {
-    public let id: UUID
-    public let description: String?
-    public let location: String?
-    public let imageURL: URL
-
-    public init(
-        id: UUID,
-        description: String?,
-        location: String?,
-        imageURL: URL
-    ) {
-        self.id = id
-        self.description = description
-        self.location = location
-        self.imageURL = imageURL
-    }
-}
