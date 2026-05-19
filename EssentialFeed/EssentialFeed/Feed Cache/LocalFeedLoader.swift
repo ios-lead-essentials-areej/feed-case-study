f87616ec921bccea08c818db2551baf7d4b8c307
Areej Sadaqa <areej.sadaqa@thechefz.co>
2026-05-19 16:19:37 +0300

    Move 'FeedCachePolicy' into its own file

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -11,27 +11,6 @@ import Foundation
 // but the caching policy its a policy so its a business rule, need to be shared across use case and across applications, so its application agnostic, maybe its a legal rule , in our case its business rule
 // we can extract the business role into a seprarted model and can be used across applications across use cases
 
-// Policy is a rule and with no identity, and we separate business logic with identity by calling them entities, so entities are models with identity and valude objects are models with no identity, in this case the policy has no identity, its just encapsulate a rule and we don't need an instance of FeedCachePolicy, it can be static
-
-private final class FeedCachePolicy {
-    private init() {}
-    
-    // but having currentDate dependencies can make it not deterministic on each use of FeedCachePolicy
-    // so instead we can make it method injection
-    private static let calender = Calendar(identifier: .gregorian)
- 
-    private static var maxCacheAgeInDays: Int {
-        return 7
-    }
-    
-    static func validate(_ timestamp: Date, against date: Date) -> Bool {
-        guard let maxCacheAge = calender.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
-            return false
-        }
-        return date < maxCacheAge
-    }
-}
-
 public final class LocalFeedLoader {
     private let store: FeedStore
     private let currentDate: () -> Date
