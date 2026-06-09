140f4506c62e2130b677fb0257d32bef163634e2
Areej Sadaqa <areej.sadaqa@thechefz.co>
2026-06-09 23:40:14 +0300

    Add 'EssentialFeedCacheIntegrationTests' target to separate the potentially-slower cache integration tests from the fast unit/isolated tests.

---
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,15 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by areej sadaqa on 09/06/2026.
+//
+
+import XCTest
+
+// the idea is to integrate all the cache module objects and see how they behave in collaboration or integrations.
+// so far we've been testing them in isolation by using test doubles but now e need to see how they behave when collaborating with real instances of the production types.
+ class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+
+}
