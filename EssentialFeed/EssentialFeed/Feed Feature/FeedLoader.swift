078a6b65663122e5c0c8c0423aeb61d20d3b679b
Areej Sadaqa <areej.sadaqa@thechefz.co>
2026-08-06 20:26:09 +0300

    Replace Custom 'LoadFeedResult' enum with the standard 'Swift Result'

--- EssentialFeed/EssentialFeed/Feed Feature/FeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedLoader.swift
@@ -8,10 +8,7 @@ import Foundation
 ///but we're only making LoadFeedResult equatable just because of the test, and this is a production code
 ///complicates alot our production codes , prod has no req of equatable 
 
-public enum LoadFeedResult {
-	case success([FeedImage])
-	case failure(Error)
-}
+public typealias LoadFeedResult = Result<[FeedImage], Error> 
 
 public protocol FeedLoader {    
 	func load(completion: @escaping (LoadFeedResult) -> Void)
