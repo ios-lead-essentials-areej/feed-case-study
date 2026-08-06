472bac0687a7c5f6f4418fb69b82025aa5adcd61
Areej Sadaqa <areej.sadaqa@thechefz.co>
2026-08-06 20:30:17 +0300

    Nest 'LoadFeedResult' into the 'FeedLoader' protocol as 'FeedLoader.Result' since they're closely related

--- EssentialFeed/EssentialFeed/Feed Feature/FeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedLoader.swift
@@ -8,8 +8,9 @@ import Foundation
 ///but we're only making LoadFeedResult equatable just because of the test, and this is a production code
 ///complicates alot our production codes , prod has no req of equatable 
 
-public typealias LoadFeedResult = Result<[FeedImage], Error> 
 
-public protocol FeedLoader {    
-	func load(completion: @escaping (LoadFeedResult) -> Void)
+public protocol FeedLoader {
+    typealias Result = Swift.Result<[FeedImage], Error>
+    
+	func load(completion: @escaping (Result) -> Void)
 }
