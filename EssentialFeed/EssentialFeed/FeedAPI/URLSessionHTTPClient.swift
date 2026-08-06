4422d0744c693c21452919a547bd043e4d8af523
Areej Sadaqa <areej.sadaqa@thechefz.co>
2026-08-06 21:46:23 +0300

    Simplify 'URLSessionHTTPClient' completion code with the new 'Result' APIs

--- EssentialFeed/EssentialFeed/FeedAPI/URLSessionHTTPClient.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/URLSessionHTTPClient.swift
@@ -21,13 +21,15 @@ public class URLSessionHTTPClient: HTTPClient {
         //the issue of our tests now its sensitive to the correct url passing because we intercept only our url, but to enhance we can intercept all reqests regarding the url
         //also we want our assertion to be more precise about the error when they fail.
         session.dataTask(with: url, completionHandler: { data , response, error in
-            if let error = error {
-                completion(.failure(error))
-            } else if let data, let response = response as? HTTPURLResponse {
-                completion(.success((data, response)))
-            } else {
-                completion(.failure(UnexpectedValuesRepresentation()))
-            }
+            completion(Result {
+                if let error = error {
+                    throw error
+                } else if let data, let response = response as? HTTPURLResponse {
+                    return (data, response)
+                } else {
+                    throw UnexpectedValuesRepresentation()
+                }
+            })
         }).resume()
     }
 }
