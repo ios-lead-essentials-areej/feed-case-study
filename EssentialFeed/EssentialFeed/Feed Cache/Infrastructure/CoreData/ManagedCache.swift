4ceea94a4b13573f5c04147183ac9d54727396ea
Areej Sadaqa <areej.sadaqa@thechefz.co>
2026-08-06 20:12:48 +0300

    Remove redundant 'internal' access control declaration

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
@@ -8,26 +8,26 @@
 import CoreData
 
 @objc(ManagedCache)
-internal class ManagedCache: NSManagedObject {
-    @NSManaged internal var timestamp: Date
-    @NSManaged internal var feed: NSOrderedSet
+ class ManagedCache: NSManagedObject {
+    @NSManaged  var timestamp: Date
+    @NSManaged  var feed: NSOrderedSet
 }
 
 extension ManagedCache {
-    internal static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+     static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
         let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
         request.returnsObjectsAsFaults = false
         return try context.fetch(request).first
     }
     
     //It enforces the invariant your store depends on: at most one ManagedCache exists at a time.
-    internal static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+     static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
         try find(in: context) //fetch → ManagedCache? (the existing cache, if any)
             .map(context.delete) // Optional.map: if non-nil, delete it; if nil, do nothing
         return ManagedCache(context: context)  // insert a fresh, empty one
     }
     
-    internal var localFeed: [LocalFeedImage] {
+     var localFeed: [LocalFeedImage] {
         return feed.compactMap { ($0 as? ManagedFeedImage)?.local }
     }
 }
